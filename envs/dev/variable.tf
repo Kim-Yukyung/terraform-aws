0c3b08eb16a63039b7ae9044f54ad30ded374129
Kim-Yukyung <yukyung116@gmail.com>
2025-07-06 15:01:44 +0900

    ✨ RDS 모듈 생성

---
+++ envs/dev/variable.tf
@@ -0,0 +1,12 @@
+# 데이터베이스 인증 정보
+variable "db_username" {
+  description = "RDS 사용자 이름"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "RDS 비밀번호"
+  type        = string
+  sensitive   = true
+}
